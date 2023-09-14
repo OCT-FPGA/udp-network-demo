32a1131f212502cf3d516a6e47c6685e277cb468
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-09-13 23:06:28 -0400

    Update host_receiver_if0.cpp

--- host/host_receiver_if0.cpp
+++ host/host_receiver_if0.cpp
@@ -158,7 +158,8 @@ int main(int argc, char **argv) {
     	rx0_run.wait();
     	buffer_packetdata.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
     	buffer_packetdata.read(local_data);
-    
+	
+        printf("Message at the receiver:\n");
     	for (unsigned int i = 0; i < packet_size_total; i++){
             	printf("%c",local_data[i]);
     	}
