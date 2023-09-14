62d3fca9400a081aaece19882fe4489ea9590174
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-09-13 23:01:42 -0400

    Update host_receiver_if3.cpp

--- host/host_receiver_if3.cpp
+++ host/host_receiver_if3.cpp
@@ -50,7 +50,7 @@ int main(int argc, char **argv) {
 	
 	socket_type sockets[2][16] = {0};
         unsigned int packet_size_total[2];
-        uint32_t rxPkt[2] = {3200, 3200};
+        uint32_t rxPkt[2] = {1, 1};
         unsigned int dec[2] = {0, 0};
 	unsigned int dest = 0;
 
